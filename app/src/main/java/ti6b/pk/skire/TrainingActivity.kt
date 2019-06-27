cf3cffb95c7c9eea866181e179d8f15e87d77fbf
vrjulianti <virariv07@gmail.com>
2019-06-27 23:53:11 +0700

    added training activity

---
+++ app/src/main/java/ti6b/pk/skire/TrainingActivity.kt
@@ -0,0 +1,12 @@
+package ti6b.pk.skire
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrainingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_training)
+    }
+}
