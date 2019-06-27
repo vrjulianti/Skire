74b3411ad3d4b89c463ccea646410c2cbd830617
vrjulianti <virariv07@gmail.com>
2019-06-28 01:17:47 +0700

    modify rv material

---
+++ app/src/main/java/ti6b/pk/skire/model/Material.kt
@@ -0,0 +1,8 @@
+package ti6b.pk.skire.model
+
+class Material (
+    var id: String? = null,
+    var text_content: String? = null,
+    var title: String? = null,
+    var video_content: String? = null
+)
